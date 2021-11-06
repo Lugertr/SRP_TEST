e3c9dfa1e6e71765863348a01698faba3e0b51eb
Lugertr <nikitavishnev@bk.ru>
2021-11-06 06:07:16 +0300

    fix g

--- src/SRP.js
+++ src/SRP.js
@@ -10,10 +10,13 @@ export default class SPR{
     Alice;
 
     constructor(l,p)
-    {  
+    {  let q,N;
         const gen = new Generate();
-        let q = gen.create(30);
-        let N = q*2n+1n;
+        do {
+        q = gen.create(10);
+        N = q*2n+1n;
+        }
+        while  (!gen.isPrime(N,gen.log2(N)))
         //let N ="0xEEAF0AB9ADB38DD69C33F80AFA8FC5E86072618775FF3C0B9EA2314C9C256576D674DF7496EA81D3383B4813D692C6E0E0D5D8E250B98BE48E495C1D6089DAD15DC7D7B46154D6B6CE8EF4AD69B15D4982559B297BCF1885C529F566660E57EC68EDBC3C05726CC02FD4CBF4976EAA9AFD5138FE8376435B9FC61D2FC0EB06E3"
 
         let k=3;
